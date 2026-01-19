a681763f313709b4db8a68ec99f70d1c0c0dc6b6
유다원 <pluralize79@gmail.com>
2026-01-19 11:39:09 +0900

    store_one_notifier 수정

--- lib/vm/store_one_notifire.dart
+++ lib/vm/store_one_notifire.dart
@@ -4,15 +4,15 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 import 'package:table_now_app/model/store.dart';
 
-class StoreNotifier extends AsyncNotifier<List<Store>> {
+class StoreOneNotifier extends AsyncNotifier<List<Store>> {
   final String baseUrl = "http://127.0.0.1:8000/api/store";
 
   @override
-  FutureOr<List<Store>> build() async {
-    return fetchStores();
+  Future<List<Store>> build() async {
+    return fetchOneStore();
   }
 
-  Future<List<Store>> fetchStores() async {
+  Future<List<Store>> fetchOneStore() async {
     try {
       final res = await http.get(Uri.parse("$baseUrl/select_stores/"));
 
@@ -34,62 +34,9 @@ class StoreNotifier extends AsyncNotifier<List<Store>> {
       throw Exception("스토어 로딩 에러: $e");
     }
   }
-
-  Future<String> insertStore(Store s) async {
-    final url = Uri.parse("$baseUrl/insert_store/");
-
-    final response = await http.post(
-      url,
-      headers: {'Content-Type': 'application/json'},
-      body: json.encode(s.toJson()),
-    );
-
-    final data = json.decode(utf8.decode(response.bodyBytes));
-
-    await refreshStores(); // 갱신
-    return data['result'].toString();
-  }
-
-  Future<String> updateStore(Store s) async {
-    final url = Uri.parse("$baseUrl/update_store/");
-
-    final response = await http.post(
-      url,
-      headers: {'Content-Type': 'application/json'},
-      body: json.encode(s.toJson()),
-    );
-
-    final data = json.decode(utf8.decode(response.bodyBytes));
-
-    await refreshStores();
-    return data['result'].toString();
-  }
-
-  Future<String> deleteStore(int seq) async {
-    final url = Uri.parse("$baseUrl/delete_store/");
-
-    final response = await http.post(
-      url,
-      headers: {'Content-Type': 'application/json'},
-      body: json.encode({'store_seq': seq}),
-    );
-
-    final data = json.decode(utf8.decode(response.bodyBytes));
-
-    await refreshStores();
-    return data['result'].toString();
-  }
-
-  Future<void> refreshStores() async {
-    state = const AsyncLoading();
-
-    state = await AsyncValue.guard(() async {
-      return fetchStores();
-    });
-  }
 }
 
 // provider 선언
-final storeNotifierProvider = AsyncNotifierProvider<StoreNotifier, List<Store>>(
-  StoreNotifier.new,
+final storeNotifierProvider = AsyncNotifierProvider<StoreOneNotifier, List<Store>>(
+  StoreOneNotifier.new,
 );
